812053c72fdba1dc425da1a01d498437dc89d831
Merite15 <86039889+Merite15@users.noreply.github.com>
2022-12-16 14:02:53 +0100

    add dependencies

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Barryvdh\Snappy\Facades\SnappyPdf as PDF;
 
 class PdfController extends Controller
 {
-    //
+    public function print(Request $request)
+    {
+        $users = User::all();
+
+        view()->share('users',$users);
+
+
+
+        if($request->has('download')) {
+
+        	// pass view file
+
+            $pdf = PDF::loadView('welcome');
+
+            // download pdf
+
+            return $pdf->download('userlist.pdf');
+
+        }
+
+        return view('welcome');
+
+    }
 }
